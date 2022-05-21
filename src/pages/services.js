b87ecd27b99a0dd9c9ceb195682bc3d8e3256304
hsantos4 <hector.santos597@gmail.com>
2022-05-21 17:49:29 -0400

    Changed the wording in the services pages

--- src/pages/services.js
+++ src/pages/services.js
@@ -96,7 +96,7 @@ const Services = () => {
                 </Col>
                 <Col className='col-text' sm={12} md={ {order:2} }>
                     <h3>Search Engine Marketing</h3>
-                    <p>Online search engine marketing makes it easier for people to find your insurance agency. We provide services to help you get in front of potential clients shopping online in just a few steps. </p>
+                    <p>Online advertising makes it easier for people to find your insurance agency. We provide Google Ad services to help you get in front of potential clients shopping online in just a few steps. </p>
                 </Col>
             </Row>
 
