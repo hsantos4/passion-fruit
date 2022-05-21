37f5ac1e1814c0adfda64369c633dd7648421b75
hsantos4 <hector.santos597@gmail.com>
2022-05-21 19:24:54 -0400

    Add the party time webworker to help with site speed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,19 @@ import './src/components/sass/base/reset.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './src/components/Layout.js';
 
+export const onRouteUpdate = ({ location }) => {
+    if (process.env.NODE_ENV !== 'production') {
+      return null;
+    }
+  
+    const pagePath = location ? location.pathname + location.search + location.hash : undefined;
+    setTimeout(() => {
+      if (typeof gtag === 'function') {
+        gtag('event', 'page_view', { page_path: pagePath });
+      }
+    }, 100);
+  };
+
 export const wrapPageElement = ({ element, props }) => {
     return <Layout {...props}>{element}</Layout>
 }
